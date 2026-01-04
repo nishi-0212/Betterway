7bc845510551218ecebfca2aecf0ae5a263902d0
Nishi Vishwakarma <132808787+nishi-0212@users.noreply.github.com>
2026-01-04 13:26:22 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const products = [
+  { id: 1, name: "T-Shirt", price: 500, category: "Clothing", stock: 5 },
+  { id: 2, name: "Jeans", price: 1200, category: "Clothing", stock: 3 },
+  { id: 3, name: "Shoes", price: 2500, category: "Clothing", stock: 0 },
+  { id: 4, name: "Laptop", price: 60000, category: "Electronics", stock: 2 },
+  { id: 5, name: "Headphones", price: 3000, category: "Electronics", stock: 4 },
+];
